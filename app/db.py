92693a1bd54475380b647a03640b07d3e581de78
SumanBanikSB <suman33banik@gmail.com>
2022-03-02 12:56:11 +0530

    "connecing name doesnot exists" error

--- app/db.py
+++ app/db.py
@@ -16,9 +16,9 @@ def get_session():
     cloud_config= {
             'secure_connect_bundle': ASTRADB_CONNECT_BUNDLE
     }
-    auth_provider = PlainTextAuthProvider('<<CLIENT ID>>', '<<CLIENT SECRET>>')
+    auth_provider = PlainTextAuthProvider(ASTRADB_CLIENT_ID, ASTRADB_CLIENT_SECRET)
     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
     session = cluster.connect()
-    connection.register_connection(str(session,session = session))
+    connection.register_connection(str(session), session=session)
     connection.set_default_connection(str(session))
     return session
